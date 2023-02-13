483c7a33e32c68aed9e150f622f1af574917c860
RodovR <rodovroman@yandex.ru>
2023-02-13 12:45:30 +0300

    Обновление для страницы авторизации, добавлены настройки для подключения css, перемещены файлы

--- Memory_Simulator/core/settings.py
+++ Memory_Simulator/core/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_PATH = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -110,9 +111,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "")
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+if __name__ == '__main__':
+    print(f'BASE_DIR >>> {BASE_DIR}')
+    print('=================================')
+    print(f'ROOT_PATH >>> {ROOT_PATH}')
+    print('=================================')
+    print(f'STATIC_ROOT >>> {STATIC_ROOT}')
