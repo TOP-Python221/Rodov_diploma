0baf2f14ae258858e036e1d1e18c353c61632482
RodovR <109357215+RodovR@users.noreply.github.com>
2023-02-23 10:46:30 +0300

    Создание первой модели - User

--- Memory_Simulator/simulator/models.py
+++ Memory_Simulator/simulator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    surname = models.CharField(max_length=30, verbose_name='Фамилия')
+    age = models.IntegerField(max_length=3, verbose_name='Возраст')
+    time_registration = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
+    last_visit = models.DateTimeField(auto_now=True, verbose_name='Последнее посещение')
+    is_online = models.BooleanField(default=False, verbose_name='В сети')
+
+    class Meta:
+        verbose_name_plural = 'Пользователи'
+        ordering = ['time_registration']
