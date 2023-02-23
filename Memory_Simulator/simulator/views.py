1d4f0e5b5d5aa9709d06814979c81081c7ca2a60
RodovR <109357215+RodovR@users.noreply.github.com>
2023-02-23 10:50:14 +0300

    Добавление контекстного меню в классы представления. Присвоение к переменной "menu" соответсвующих разделов для работы с шаблонами

--- Memory_Simulator/simulator/views.py
+++ Memory_Simulator/simulator/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 
+
+menu = [{'title': 'Статистика', 'url_name': 'statistics'},
+        {'title': 'Рейтинги', 'url_name': 'ratings'},
+        {'title': 'Тренажеры', 'url_name': 'simulators'},
+        {'title': 'Авторизация', 'url_name': 'authorization'}
+        ]
+
 def index_view(request):
     return render(
         request,
-        'pages/main.html'
+        'pages/main.html',
+        {'title': 'Тренажеры', 'menu': menu}
     )
 
 def login_view(request):
     return render(
         request,
-        'pages/login.html'
+        'pages/login.html',
+        {'title': 'Авторизация'}
     )
 
 def registration_view(request):
     return render(
         request,
-        'pages/registration.html'
+        'pages/registration.html',
+        {'title': 'Регистрация'}
     )
