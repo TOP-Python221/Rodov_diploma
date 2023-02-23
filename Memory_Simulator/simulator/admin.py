b88f4f702b044ab0147c67f7b5cc7f357d552526
RodovR <109357215+RodovR@users.noreply.github.com>
2023-02-23 10:42:21 +0300

    Работа с админ-панелью. Регистрация условного пользователя и суперпользователя

--- Memory_Simulator/simulator/admin.py
+++ Memory_Simulator/simulator/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'surname', 'age',
+                    'time_registration', 'last_visit', 'is_online',)
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'is_online')
+
+admin.site.register(User, UserAdmin)
