9b3e64505987e25fab69cc35a73b74bac20a7cd5
Gennadiy <renardes@gmail.com>
2023-02-26 13:35:41 +0500

    комментарии и исправления

--- Memory_Simulator/simulator/models.py
+++ Memory_Simulator/simulator/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     surname = models.CharField(max_length=30, verbose_name='Фамилия')
-    age = models.IntegerField(max_length=3, verbose_name='Возраст')
+    # ИСПОЛЬЗОВАТЬ: в модели ограничение задаётся только типом поля
+    # КОММЕНТАРИЙ: если необходимо добавить дополнительное ограничение, то мы его вводим для соответствующего поля формы с помощью параметров min_value и max_value
+    age = models.PositiveSmallIntegerField(verbose_name='Возраст')
     time_registration = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
     last_visit = models.DateTimeField(auto_now=True, verbose_name='Последнее посещение')
     is_online = models.BooleanField(default=False, verbose_name='В сети')
--- Memory_Simulator/simulator/views.py
+++ Memory_Simulator/simulator/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 
 
-menu = [{'title': 'Статистика', 'url_name': 'statistics'},
-        {'title': 'Рейтинги', 'url_name': 'ratings'},
-        {'title': 'Тренажеры', 'url_name': 'simulators'},
-        {'title': 'Авторизация', 'url_name': 'authorization'}
-        ]
+# ИСПОЛЬЗОВАТЬ: возможно создать словарь с доступом к ключам с помощью атрибуции
+
+class DictAttr(dict):
+    def __getattribute__(self, item):
+        return self[item]
+
+menu = [
+    DictAttr({'title': 'Статистика', 'url_name': 'statistics'}),
+    DictAttr({'title': 'Рейтинги', 'url_name': 'ratings'}),
+    DictAttr({'title': 'Тренажеры', 'url_name': 'simulators'}),
+    DictAttr({'title': 'Авторизация', 'url_name': 'authorization'})
+]
+
+# {% for item in menu %}
+#   <a href="{{ item.url }}">{{ item.title }}</a>
+# {% endfor %}
+
 
 def index_view(request):
     return render(
@@ -27,3 +39,17 @@ def registration_view(request):
         'pages/registration.html',
         {'title': 'Регистрация'}
     )
+
+
+def game_view(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            '',
+            {}
+        )
+    elif request.method == 'POST':
+        form = GameForm(request.POST)
+        if form.is_valid():
+            scores = form.cleaned_data['scores']
+            # ...
