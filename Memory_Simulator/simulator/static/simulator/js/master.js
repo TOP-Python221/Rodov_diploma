9044f9c3ff8d73d5f557631e744fe490265ea2e3
RodovR <109357215+RodovR@users.noreply.github.com>
2023-03-25 15:39:20 +0300

    Добавление таймера и счётчика на страницу с игрой

---
+++ Memory_Simulator/simulator/static/simulator/js/master.js
@@ -0,0 +1,39 @@
+    // ТАЙМЕР
+    var h1 = document.getElementsByTagName('h1')[0];
+	var sec = 0;
+	var min = 0;
+	var hrs = 0;
+	var t;
+
+		function tick(){
+    		sec++;
+    		if (sec >= 60) {
+        		sec = 0;
+        		min++;
+        		if (min >= 60) {
+            		min = 0;
+            		hrs++;
+        		}
+    		}
+		}
+
+		function add() {
+    		tick();
+    		h1.textContent = (hrs > 9 ? hrs : "0" + hrs) + ":" + (min > 9 ? min : "0" + min) + ":" + (sec > 9 ? sec : "0" + sec);
+    		timer();
+		};
+		function timer() {
+    		t = setTimeout(add, 1000);
+		};
+
+    // СЧЁТЧИК
+	var count = 0;
+
+	function counter(){
+	    updateCount(++count);
+	};
+
+	function updateCount(value){
+	    console.log(value);
+	    document.getElementById('game-id').innerHTML = value;
+	};
