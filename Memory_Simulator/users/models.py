a26163be06b3a0cbe0888a8bd57d7efcf92d1fbd
RodovR <109357215+RodovR@users.noreply.github.com>
2023-04-05 12:11:50 +0300

    Попытка унаследовать метод is_authenticated от родителя

--- Memory_Simulator/users/models.py
+++ Memory_Simulator/users/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 from .manager import CustomUserManager
-from django.conf import settings
 
 
 class RegUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=55, blank=True, null=True)
     email = models.EmailField('email address', unique=True)
-    age = models.PositiveSmallIntegerField(verbose_name='Возраст')
+    age = models.PositiveSmallIntegerField(verbose_name='Возраст', blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     last_visit = models.DateTimeField(auto_now=True, verbose_name='Последнее посещение')
@@ -19,18 +19,16 @@ class RegUser(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
-        return self.email
-
-
     class Meta:
         verbose_name_plural = 'Пользователи'
         ordering = ['date_joined']
 
+    # def is_authenticated(self):
+    #     return True
 
-# КОММЕНТАРИЙ: наследование от класса User возможно (точнее, необходимо наследоваться от AbstractUser), но это требует дополнительного указания для фреймворка о необходимости использовать для авторизации и аутентификации ваш класс, а не встроенный: AUTH_USER_MODEL = RegUser
-#   см. https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#substituting-a-custom-user-model
+    def get_is_authenticated(self):
+        auth = super().is_authenticated()
+        return auth
 
-# КОММЕНТАРИЙ: альтернативой является использование агрегации вместо наследования, то есть связь собственного класса для пользователя со встроенным с помощью models.OneToOneField
-#   см. https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#extending-the-existing-user-model
-
+    def __str__(self):
+        return self.email
