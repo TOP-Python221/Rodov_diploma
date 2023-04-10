213882eee421ad713e3cfd654302571bace1f821
RodovR <109357215+RodovR@users.noreply.github.com>
2023-04-10 13:58:24 +0300

    Регистрация админ-панели приложения posts

--- Memory_Simulator/posts/admin.py
+++ Memory_Simulator/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('time_create', )
+
+
+admin.site.register(Posts, PostsAdmin)
